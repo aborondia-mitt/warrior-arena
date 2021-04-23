83053c39050f9f02c90a8fc40da4615df2a13244
Andrew Borondia <andrew>
2021-04-22 20:14:28 -0500

    replace novicell animator with self written animate code

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,11 @@
 const gameData = {
+  // ctx: canvas.getContext('2d'),
+  // width: canvas.width = window.innerWidth,
+  // height: canvas.height = window.innerHeight,
+  // playerContainer: document.querySelector('player-container'),
   mainContainer: document.querySelector('.main'),
-  playerModel: document.getElementById('player'),
+  player: document.getElementById('player'),
+  // playerModel: document.getElementById('player'),
   enemyModel: document.getElementById('enemy'),
   attackButton: document.getElementById('attack'),
   defendButton: document.getElementById('defend'),
@@ -11,39 +16,29 @@ const gameData = {
   moveSpeed: 30,
   moveDuration: 500,
 }
+canvas = document.querySelector('canvas');
+ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-const Player = function (name, health, attackPower, defense, model, poses, voiceSet) {
-  this.name = name;
-  this.health = health;
-  this.attackPower = attackPower;
-  this.defense = defense;
-  this.characterModel = model;
-  this.poses = poses;
-  this.voiceSet = voiceSet;
-  this.startingX = '400px';
-  this.startingY = '100px';
-  this.xMoveFactor = 10;
-}
 
-const Enemy = function (name, health, attackPower, defense, model, poses, voiceSet) {
-  Player.call(this);
-  this.name = name;
-  this.health = health;
-  this.attackPower = attackPower;
-  this.defense = defense;
-  this.characterModel = model;
-  this.poses = poses;
-  this.voiceSet = voiceSet;
-  this.startingX = '1000px';
-  this.startingY = '100px';
-  this.xMoveFactor = -10;
-}
+class Player {
+  constructor(name, health, attackPower, defense, model, animationSheet, voiceSet) {
+    this.name = name;
+    this.health = health;
+    this.attackPower = attackPower;
+    this.defense = defense;
+    this.characterModel = model;
+    this.animationSheet = animationSheet;
+    this.voiceSet = voiceSet;
+    this.startingX = '400px';
+    this.startingY = '100px';
+    this.xMoveFactor = 10;
+    this.animations = new Image();
+    this.animations.src = animationSheet;
 
-Enemy.prototype = Object.create(Player.prototype);
-Enemy.prototype.constructor = Enemy;
-
-const playerMethods = [
-  Player.prototype.changeStance = function (character, stance) {
+  }
+  changeStance(character, stance) {
     // 1: Idle; 2: Ready; 3: Attack; 4: Special;
     // 5: Defend; 6: Flinch;  7: Victory; 8: Fallen;
     for (i = 0; i < character.poses.length; i++) {
@@ -51,21 +46,25 @@ const playerMethods = [
         character.characterModel.src = character.poses[i][1];
       }
     }
-  },
-  Player.prototype.moveForward = function (character) {
+  }
+
+  moveForward(character) {
     const startPosition = parseInt(character.characterModel.style.left, 10);
     character.characterModel.style.left = (startPosition + character.xMoveFactor) + "px";
-  },
-  Player.prototype.commenceAttack = function (character) {
+  }
+
+  commenceAttack(character) {
     setTimeout(function () {
       character.changeStance(character, 'attack');
     }, gameData.moveDuration / 3);
-  },
-  Player.prototype.moveBack = function (character) {
+  }
+
+  moveBack(character) {
     const startPosition = parseInt(character.characterModel.style.left, 10);
     character.characterModel.style.left = (startPosition - character.xMoveFactor) + "px";
-  },
-  Player.prototype.attack = function (character) {
+  }
+
+  attack(character) {
     character.changeStance(character, 'ready');
 
     const moveForward = setInterval(function () {
@@ -86,40 +85,41 @@ const playerMethods = [
         clearInterval(moveBack);
       }, gameData.moveDuration)
     }, gameData.moveDuration)
-  },
-  Player.prototype.defend = function (character) {
+  }
+
+  defend(character) {
     character.changeStance(character, 'defend');
 
     setTimeout(function () {
       character.changeStance(character, 'idle');
     }, gameData.moveDuration)
-  },
+  }
 
-  Player.prototype.flinch = function (character) {
+  flinch(character) {
     character.changeStance(character, 'flinch');
 
     setTimeout(function () {
       character.changeStance(character, 'idle');
     }, gameData.moveDuration)
-  },
+  }
 
-  Player.prototype.victory = function (character) {
+  victory(character) {
     character.changeStance(character, 'victory');
 
     setTimeout(function () {
       character.changeStance(character, 'idle');
     }, gameData.moveDuration)
-  },
+  }
 
-  Player.prototype.fall = function (character) {
+  fall(character) {
     character.changeStance(character, 'fall');
 
     setTimeout(function () {
       character.changeStance(character, 'idle');
     }, gameData.moveDuration)
-  },
+  }
 
-  Player.prototype.evade = function (character) {
+  evade(character) {
     const moveBack = setInterval(function () {
       character.moveBack(character);
     }, gameData.moveSpeed);
@@ -134,12 +134,44 @@ const playerMethods = [
         clearInterval(moveForward);
       }, gameData.moveDuration)
     }, gameData.moveDuration)
-  },
-]
+  }
 
-const createPlayerMethods = function () {
-  for (method of playerMethods) {
-    Player.prototype.method = method;
+  moveHorizontal () {
+    setInterval(function () {
+      ctx.translate(-.5, 0);
+    }, 1);
+  }
+
+  animateCharacter() {
+    let currentFrame = 0;
+    const frameWidth = 250;
+    const frameHeight = 193.2;
+    setInterval(function () {
+  
+      let numColumns = 4;
+      let numRows = 1;
+      currentFrame++;
+  
+      if (currentFrame > numColumns) {
+        currentFrame = 0;
+      }
+  
+      let column = currentFrame % numColumns;
+      let row = Math.floor(currentFrame / numColumns);
+  
+      ctx.clearRect(10, 10, canvas.width, canvas.height);//clears previous animation frame
+      ctx.drawImage(player.animations, column * frameWidth, (row + 5) * frameHeight, frameWidth, frameHeight, 10, 30, frameWidth, frameHeight);
+  
+    }, 200);
+  }
+}
+
+class Enemy extends Player {
+  constructor(name, health, attackPower, defense, model, animations, voiceSet) {
+    super(name, health, attackPower, defense, model, animations, voiceSet);
+    this.name = name;
+    this.startingX = '1000px';
+    this.startingY = '100px';
   }
 }
 
@@ -204,9 +236,29 @@ gameData.mainContainer.addEventListener('click', function (event) {
   determineClickResult(target);
 })
 
-createPlayerMethods();
-const player = new Player('player', 50, 10, 5, gameData.playerModel, populateCharacterPoses('player'), []);
-const enemy = new Enemy('enemy', 50, 10, 5, gameData.enemyModel, populateCharacterPoses('enemy1'), []);
-addCharactersToScreen();
+// const player = new Player('player', 50, 10, 5, gameData.playerModel, populateCharacterPoses('player'), []);
+const player = new Player('player', 50, 10, 5, gameData.player, ['images/grayanim.png'], []);
+// const enemy = new Enemy('enemy', 50, 10, 5, gameData.enemyModel, populateCharacterPoses('enemy1'), []);
+// addCharactersToScreen();
 
 //try position offset with constant setinterval update
+// const animatePlayer = new SpriteAnimation({
+//   element: "player",
+//   frames: 4,
+//   duration: 600,
+//   columns: 4,
+//   rows: 1,
+//   iterations: 1
+// });
+
+// animatePlayer.animateSprite();
+
+
+
+
+//location on canvas
+ctx.translate(canvas.width / 7, canvas.height / 1.6);
+//flip horizontal
+ctx.scale(-1, 1);
+
+player.animateCharacter();
